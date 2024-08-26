a1aa412ce3af67ce9e2c4ecd2f3690404082c9b9
Rascarbo <142728181+Rascarbo@users.noreply.github.com>
2024-08-26 09:52:38 +0200

    Change first function to switch statement Add rock paper scissors cases selected with math.random

--- javascript.js
+++ javascript.js
@@ -1,6 +1,13 @@
 //function randomly generate number between 1 and 3
 //function return rock, paper or scissors
 function getComputerChoice(){
-    return Math.floor(Math.random()*3+1)
+    switch (Math.floor(Math.random()*3+1)) {
+        case 1:
+            return "Rock";
+        case 2:
+            return "Paper";
+        case 3:
+            return "Scissors";
+    }
 }
 console.log(getComputerChoice())
