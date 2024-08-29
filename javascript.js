c1b14d7e842e28a36700ee0d5fb88cc0cb81cccf
Rascarbo <142728181+Rascarbo@users.noreply.github.com>
2024-08-29 12:26:21 +0200

    Compare both answers and return the winner Used or and operators with if to compare and switch to find the winner. Also added console.log to see the results

--- javascript.js
+++ javascript.js
@@ -16,6 +16,7 @@ function getComputerChoice(){
 function getHumanChoice(){
     let humanChoice = prompt("Rock, Paper or Scissors?");
     humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1).toLowerCase();
+    return humanChoice;
 }
 
 //Initialize human and computer scores
@@ -28,6 +29,49 @@ let computerScore = 0;
 //Output results
 //Add points to the winner
 function playRound(humanChoice, computerChoice){
-
+    if (humanChoice == computerChoice){
+            console.log("Draw!");
+    } else if(humanChoice === "Rock" || computerChoice === "Rock" && humanChoice === "Paper" || computerChoice === "Paper"){
+        switch ("Paper"){
+            case humanChoice:
+                console.log("You win, Paper beats Rock!");
+                humanScore++;
+                break;
+            case computerChoice:
+                console.log("You lose, Paper beats Rock!")
+                computerScore++;
+                break;
+        }
+    } else if(humanChoice === "Rock" || computerChoice === "Rock" && humanChoice === "Scissors" || computerChoice === "Scissors"){
+        switch ("Rock"){
+            case humanChoice:
+                console.log("You win, Rock beats Scissors!");
+                humanScore++;
+                break;
+            case computerChoice:
+                console.log("You lose, Rock beats Scissors!");
+                computerScore++;
+                break;
+        }
+    } else if (humanChoice === "Paper" || computerChoice === "Paper" && humanScore === "Scissors" || computerChoice === "Scissors"){
+        switch ("Scissors"){
+            case humanChoice:
+                console.log("You win, Scissors beats Paper!");
+                humanScore++;
+                break;
+            case computerChoice:
+                console.log("You lose, Scissors beats Paper!");
+                computerScore++;
+                break;
+        }
+    } else{
+        console.log("Wrong input, try again!");
+    }
 }
 
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+console.log(computerSelection);
+console.log(humanSelection);
+
+playRound(humanSelection, computerSelection);
